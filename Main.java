c5c5a644f4c2fc5671c0158d83022872ff21dd2d
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-11-22 22:54:33 +0530

    Added:Maximum among Three Integer

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ package com.bridgelabz;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to check maximum among three integer.");
+		System.out.println("Welcome to check max of three variables");
+
+		Integer a = 25, b = 24, c = 19;
+
+		TestMaximum.testMaximum(a, b, c);
+
 	}
 }
